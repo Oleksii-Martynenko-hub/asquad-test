525b94e1a827d8270b01b7a785e9c0b99fc6bf47
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2022-12-21 06:56:13 +0200

    Add hooks

---
+++ src/components/hooks/useTimeout.ts
@@ -0,0 +1,42 @@
+import { useCallback, useEffect, useRef } from 'react'
+
+export const useTimeout = (callback: () => any, delay: number) => {
+  const firstRenderRef = useRef(true)
+  const callbackRef = useRef(callback)
+  const timeoutRef = useRef<NodeJS.Timeout>()
+  const delayRef = useRef<number>()
+
+  useEffect(() => {
+    callbackRef.current = callback
+  }, [callback])
+
+  const set = useCallback(() => {
+    if (firstRenderRef.current) {
+      firstRenderRef.current = false
+      return
+    }
+    console.log('setTimeout', 'sec: ', delay / 1000)
+    timeoutRef.current = setTimeout(() => callbackRef.current(), delay)
+  }, [delay])
+
+  const clear = useCallback(() => {
+    timeoutRef.current && clearTimeout(timeoutRef.current)
+  }, [])
+
+  useEffect(() => {
+    set()
+    return clear
+  }, [delay, set, clear])
+
+  const reset = useCallback(() => {
+    clear()
+    set()
+  }, [clear, set])
+
+  const setDelay = useCallback((delay: number) => {
+    clear()
+    set()
+  }, [clear, set])
+
+  return [reset, clear]
+}
